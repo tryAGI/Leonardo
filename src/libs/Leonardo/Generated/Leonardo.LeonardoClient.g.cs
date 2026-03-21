927e7bc7452e6d31c6966dd2822e23d2443fa974
Konstantin S. <havendv@gmail.com>
2026-03-21 05:27:23 +0400

    feat: Updated OpenAPI spec (#144)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Leonardo/Generated/Leonardo.LeonardoClient.g.cs
+++ src/libs/Leonardo/Generated/Leonardo.LeonardoClient.g.cs
@@ -85,7 +85,7 @@ namespace Leonardo
         /// <summary>
         /// 
         /// </summary>
-        public MotionClient Motion => new MotionClient(HttpClient, authorizations: Authorizations)
+        public ModelsClient Models => new ModelsClient(HttpClient, authorizations: Authorizations)
         {
             ReadResponseAsString = ReadResponseAsString,
             JsonSerializerContext = JsonSerializerContext,
@@ -94,7 +94,7 @@ namespace Leonardo
         /// <summary>
         /// 
         /// </summary>
-        public ModelsClient Models => new ModelsClient(HttpClient, authorizations: Authorizations)
+        public MotionClient Motion => new MotionClient(HttpClient, authorizations: Authorizations)
         {
             ReadResponseAsString = ReadResponseAsString,
             JsonSerializerContext = JsonSerializerContext,
